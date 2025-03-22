2bf7c7fda72a55692a087b5e955f3236b27e3945
Leonoek <3292976270@qq.com>
2025-03-22 17:26:29 +0800

    change

--- C_C++/exercise/LeetCode_zuochengyun_2019/pre/heap_sort.c
+++ C_C++/exercise/LeetCode_zuochengyun_2019/pre/heap_sort.c
@@ -7,63 +7,136 @@
 #include <stdlib.h>
 
 void swap(int* array, int a, int b){
-    array[a] ^= array[b];
-    array[b] ^= array[a];
-    array[a] ^= array[b];
+    int tmp = array[a];
+    array[a] = array[b];
+    array[b] = tmp;
 }
 
-void process1(int* array, int head, int end){
-    int size = end - head + 1;
-    int p = end;
-    int parent;
-    int tmp;
-    while (p != head)
+void mysiftDown(int* array, int index, int num){
+    int left;
+    int right;
+    int target;
+
+    while (1)
     {
-        tmp = p;
-        parent = (tmp - 1) / 2;
-        while (array[tmp] > array[parent] && tmp != head)
+        left = index * 2 + 1;
+        right = index * 2 + 2;
+        target = index;
+
+        if (left < num && array[left] > array[target])
         {
-            swap(array, tmp, parent);
-            tmp = parent;
-            parent = (tmp - 1) / 2;
+            target = left;
         }
-        --p;
+
+        if (right < num && array[right] > array[target])
+        {
+            target = right;
+        }
+        
+        if (target == index)
+        {
+            break;
+        }
+        
+        swap(array, target, index);
+        index = target;
+    }
+}
+
+void mysiftUp(int* array, int index){
+    int parent;
+    int target;
+
+    while (1)
+    {
+        parent = (index - 1) / 2;
+        target = index;
+
+        if (parent > -1 && array[parent] < array[index])
+        {
+            target = parent;
+        }
+        
+        if (target == index)
+        {
+            break;
+        }
+        
+        swap(array, target, index);
+        index = parent;
+    }
+}
+
+void heap_sort1(int* input_array1, int array_num){
+    for (int i = array_num / 2 - 1; i > -1; i--)
+    {
+        mysiftDown(input_array1, i, array_num);
+    }
+
+    printf("first shift down the array:\n");
+    for (int i = 0; i < array_num; i++)
+    {
+        printf("%d ", input_array1[i]);
+    }
+
+    for (int i = array_num - 1; i > 0; i--)
+    {
+        swap(input_array1, 0, i);
+        mysiftDown(input_array1, 0, i);
+    }
+    
+    printf("\nsecond shift down the array:\n");
+    for (int i = 0; i < array_num; i++)
+    {
+        printf("%d ", input_array1[i]);
+    }
+}
+
+void heap_sort2(int* input_array, int array_num){
+    for (int i = array_num - 1; i > 0; i--)
+    {
+        mysiftUp(input_array, i);
+    }
+
+    printf("\nfirst shift down the array:\n");
+    for (int i = 0; i < array_num; i++)
+    {
+        printf("%d ", input_array[i]);
+    }
+
+    for (int i = array_num - 1; i > 0; i--)
+    {
+        swap(input_array, 0, i);
+        mysiftDown(input_array, 0, i);
+    }
+    
+    printf("\nsecond shift down the array:\n");
+    for (int i = 0; i < array_num; i++)
+    {
+        printf("%d ", input_array[i]);
     }
 }
 
 int main(){
     srand((unsigned)time(NULL));
-    int array_num = rand() % 10;
+    int array_num = 9;
     while (!array_num)
     {
         array_num = (rand() % 10);
     }
 
-    int input_array1[array_num];
-    for (int i = 0; i < array_num; i++)
-    {
-        input_array1[i] = rand() % 10;
-        printf("%d ", input_array1[i]);
-    }
-    printf("\n");
-
-    // 对数器
-    // int input_array2[array_num];
-    // memcpy(input_array2, input_array1, array_num * sizeof(int));
-
-    process1(input_array1, 0, array_num - 1);
-    // process2(input_array2, 0, array_num - 1);
-    
-    // int result = memcmp(input_array1, input_array2, array_num);
-    // if (result != 0)
+    // int input_array1[array_num];
+    // for (int i = 0; i < array_num; i++)
     // {
-    //     printf("error!\n");
+    //     input_array1[i] = rand() % 10;
+    //     printf("%d ", input_array1[i]);
     // }
+    // printf("\n");
+    int input_array1[] = {0, 7, 7, 7, 9, 5, 2, 9, 8};
+    heap_sort1(input_array1, array_num);
+
+    int input_array2[] = {0, 7, 7, 7, 9, 5, 2, 9, 8};
+    heap_sort2(input_array2, array_num);
     
-    printf("the array:\n");
-    for (int i = 0; i < array_num; i++)
-    {
-        printf("%d ", input_array1[i]);
-    }
     return 0;
 }
