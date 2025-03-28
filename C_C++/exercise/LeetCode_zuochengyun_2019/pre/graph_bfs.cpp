14a8e4ff4da1d5ec2ff6f31493c9f97f8f9a32fd
Leonoek <3292976270@qq.com>
2025-03-28 11:07:33 +0800

    add

---
+++ C_C++/exercise/LeetCode_zuochengyun_2019/pre/graph_bfs.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <memory>
+#include <map>
+#include <set>
+#include <vector>
+#include <deque>
+
+class Edge;
+
+class Node
+{
+public:
+    int value;
+    int in;
+    int out;
+    std::vector<std::shared_ptr<Node>> nexts;
+    std::vector<std::shared_ptr<Edge>> edges;
+    Node(int val) :value{val}, in{0}, out{0}{};
+    ~Node(){};
+};
+
+class Edge
+{
+public:
+    int weight;
+    std::shared_ptr<Node> from;
+    std::shared_ptr<Node> to;
+    Edge(int weight, std::shared_ptr<Node> from, std::shared_ptr<Node> to) : weight{weight}, from{from}, to{to}{};
+    ~Edge(){};
+};
+
+class Graph
+{
+public:
+    std::map<int, std::shared_ptr<Node>> nodes;
+    std::set<std::shared_ptr<Edge>> edges;
+    Graph(/* args */){};
+    ~Graph(){};
+};
+
+std::shared_ptr<Graph> createGraph(const std::vector<std::vector<int>>& input_vector){
+    std::shared_ptr<Graph> graph = std::make_shared<Graph>();
+    for (int i = 0; i < input_vector.size(); i++)
+    {
+        int weight = input_vector[i][0];
+        int from = input_vector[i][1];
+        int to = input_vector[i][2];
+
+        if (graph->nodes.find(from) == graph->nodes.end())
+        {
+            graph->nodes.emplace(from, std::make_shared<Node>(from));
+        }
+
+        if (graph->nodes.find(to) == graph->nodes.end())
+        {
+            graph->nodes.emplace(to, std::make_shared<Node>(to));
+        }
+        
+        std::shared_ptr<Node> fromNode = graph->nodes[from];
+        std::shared_ptr<Node> toNode = graph->nodes[to];
+        std::shared_ptr<Edge> new_Edge = std::make_shared<Edge>(weight, fromNode, toNode);
+        fromNode->nexts.push_back(toNode);
+        ++fromNode->out;
+        ++toNode->in;
+        fromNode->edges.push_back(new_Edge);
+        graph->edges.emplace(new_Edge);
+    }
+    return graph;
+}
+
+void bfs(std::shared_ptr<Node>& input_node){
+    if (!input_node)
+    {
+        return;
+    }
+    
+    std::set<std::shared_ptr<Node>> set;
+    std::deque<std::shared_ptr<Node>> deque;
+
+    set.emplace(input_node);
+    deque.push_front(input_node);
+    while (deque.size())
+    {
+        for (auto i : deque.front()->nexts)
+        {
+            if (set.find(i) == set.end())
+            {
+                set.emplace(i);
+                deque.push_back(i);
+            }
+        }
+        std::cout << deque.front()->value << std::endl;
+        deque.pop_front();
+    } 
+}
+
+int main() {
+    std::vector<std::vector<int>> vector1 {
+        {5, 1, 2},
+        {2, 1, 3},
+        {4, 9, 6}
+    };
+
+    std::shared_ptr<Graph> graph = createGraph(vector1);
+    bfs(graph->nodes.begin()->second);
+
+    return 0;
+}
